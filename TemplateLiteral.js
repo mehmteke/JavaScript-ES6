ebc39d4d5651074c84a13352eb11327f6b3c9696
Mehmet Teker <teker.mehmed@gmail.com>
2020-04-04 15:54:08 +0300

    How to creating dynamic html in .js

--- TemplateLiteral.js
+++ TemplateLiteral.js
@@ -4,11 +4,33 @@ const salary = 4500;
 
 // ES6 öncesi kullanım
 const a = "İsim :" + name + "\n" + " Bölüm :" + department + "\n" + " Maaş : " + salary;
-console.log(a);
+//console.log(a);
 
 /*
  BackTicks kullanara Ctrl+alt + ; veya alt-gr + 2*;
 */
 //  Yeni Kullanım
 const b = `İsim : ${name} \n Bölüm : ${department} \n Maaş:${salary}`;
-console.log(b);
+//console.log(b);
+
+const html = "<ul> " +
+             "<li> " + name + "</li>" +
+             "<li> " + department + "</li>" +
+             "<li> " + salary + "</li>" +
+             "</ul>";
+             // document.body.innerHTML = html;
+             
+const newHtml = `
+         <ul>
+             <li> ${name} </li>
+             <li> ${department}</li>
+             <li> ${salary}</li>
+         </ul> 
+`; 
+ 
+document.body.innerHTML = newHtml;
+
+
+
+
+
