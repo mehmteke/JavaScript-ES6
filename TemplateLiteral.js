1e04546abff7a3cb81e9a269d66e445c4ac46d8d
Mehmet Teker <teker.mehmed@gmail.com>
2020-04-04 15:33:32 +0300

    Template Literal String Oluşturma Yeni Standart

---
+++ TemplateLiteral.js
@@ -0,0 +1,14 @@
+const name = "Mehmet";
+const department = "Developer";
+const salary = 4500;
+
+// ES6 öncesi kullanım
+const a = "İsim :" + name + "\n" + " Bölüm :" + department + "\n" + " Maaş : " + salary;
+console.log(a);
+
+/*
+ BackTicks kullanara Ctrl+alt + ; veya alt-gr + 2*;
+*/
+//  Yeni Kullanım
+const b = `İsim : ${name} \n Bölüm : ${department} \n Maaş:${salary}`;
+console.log(b);
