2ed7946de33b85619faa3b066d3eaeeb1c040561
Mehmet Teker <teker.mehmed@gmail.com>
2020-04-05 15:27:59 +0300

    ES6+ Özellikleri Arrow Function

---
+++ ES6+Özellikleri/Arrow Function/arrow.js
@@ -0,0 +1,14 @@
+// ES6 öncesi function tanımı
+const merhaba = function(){
+    console.log("Merhaba");
+}
+merhaba();
+
+
+// Arrow function
+
+const newMerhaba = () => {
+    console.log("Arrow Function");
+}
+
+newMerhaba();
